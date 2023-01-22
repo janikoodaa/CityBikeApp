bc7e79f86dc2d4b54bbd95aa3223491e3a44b7d0
Jani Kaikkonen <janikaikkonen@live.com>
2023-01-22 09:51:48 +0200

    Limit new trip to be max one week old.

--- CityBikeAPI/CityBikeAPI/Attributes/DepartureDateValidation.cs
+++ CityBikeAPI/CityBikeAPI/Attributes/DepartureDateValidation.cs
@@ -13,10 +13,15 @@ namespace CityBikeAPI.Attributes
             {
                 return new ValidationResult("Departure date is mandatory.");
             }
-            var model = (Models.NewTripIn)validationContext.ObjectInstance;
             DateTime _departureDate = Convert.ToDateTime(value);
-            DateTime _returnDate = Convert.ToDateTime(model.ReturnDate);
+            //if (_departureDate < new DateTime(2021, 1, 1))
+            if (_departureDate < DateTime.Now.AddDays(-7))
+            {
+                return new ValidationResult("Departure date must be within week from this moment.");
+            }
 
+            var model = (Models.NewTripIn)validationContext.ObjectInstance;
+            DateTime _returnDate = Convert.ToDateTime(model.ReturnDate);
 
             if ((_returnDate - _departureDate).TotalSeconds < 10)
             {
